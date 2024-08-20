69180f050376a4693115ada988245c5b34bcdfeb
Victor Didoff <victor.didoff2003@gmail.com>
2024-08-19 22:21:31 -0300

    Main Logic Finished

--- Program.cs
+++ Program.cs
@@ -10,11 +10,13 @@ namespace CreateProtocol
         {
             string name = "CreateProtocol";
             ProtocolController? protocol = new();
-            _ = protocol.Execute(Method.Install, name);
             if (args.Length > 0)
             {
                 Console.WriteLine($"Call From Protocol:\n{args[0]}");
                 _ = protocol.Execute(Method.Uninstall, name);
+            } else {
+                _ = protocol.Execute(Method.Install, name);
+                Console.WriteLine("Press any key to exit.");
             }
             Console.ReadKey();
             return;
